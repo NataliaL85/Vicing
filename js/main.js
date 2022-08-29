7d7e80af75ee062b250ff0ba8cc0ab8ebd8fc765
NataliaL85 <099666nl@gmail.com>
2022-08-30 01:08:36 +0300

    last commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 $(function(){
 
-     $('[data-fancybox]').fancybox({
-         youtube: {
-             controls: 0,
-             showinfo: 0
-         }  
-     });
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
 
 
      $('.heroes__slider-img').slick({
@@ -14,8 +12,17 @@ $(function(){
         asNavFor: '.heroes__slider-text',
         prevArrow:'<button class=" slick-btn slick-prev"><img src="images/erow-left.png" alt="prev"></button>',
         nextArrow:'<button class=" slick-btn slick-next"><img src="images/erow-right.png" alt="next"></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+               arrows:false,
+              }
+            }
+        ]
         
     });
+   
     $('.heroes__slider-text').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
